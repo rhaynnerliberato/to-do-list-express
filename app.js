bc5f9ebade87874b3df5ab585a234720deaebf31
Rhaynner Liberato <rhay_liberato@hotmail.com>
2021-09-28 09:42:55 -0300

    First page EJS

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express')
+const path = require('path')
+
 const checkListRouter = require('./src/routes/checklist')
+const rootRouter = require('./src/routes/index')
+
 require('./config/database')
 
-
 const app = express()
-
-//middleware json
 app.use(express.json())
 
+app.set('views', path.join(__dirname, 'src/views'))
+app.set('view engine', 'ejs')
+
+app.use('/', rootRouter)
 app.use('/checklist', checkListRouter)
 
 app.listen(3000, () => {
