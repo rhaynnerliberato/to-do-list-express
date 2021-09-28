cf3feeee14e751cc106afe09f67b2a4742271dfd
Rhaynner Liberato <rhay_liberato@hotmail.com>
2021-09-28 11:07:02 -0300

    data via routes

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -6,19 +6,19 @@ const Checklist = require('../models/checklist')
 
 router.get('/', async (req, res) => {
     try {
-        let checklist = await Checklist.find({})
-        res.status(200).json(checklist)
+        let checklists = await Checklist.find({})
+        res.status(200).render('checklists/index', { checklists: checklists })
     } catch (error) {
-        res.status(500).json(error)
+        res.status(500).render('pages/error', {error: 'Erro ao exibir as listas'})
     }
 })
 
 router.get('/:id', async (req, res) => {
     try {
         let checklist = await Checklist.findById(req.params.id)
-        res.status(200).json(checklist)
+        res.status(200).render('checklists/show', { checklist: checklist })
     } catch (error) {
-        res.status(422).json(error)
+        res.status(422).render('pages/error', {error: 'Erro ao exibir as listas de tarefas'})
     }
 })
 
