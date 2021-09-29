4ef770b43f34967e08da82fb9590ea12a0d5a4d1
Rhaynner Liberato <rhay_liberato@hotmail.com>
2021-09-28 21:03:51 -0300

    Tarefas

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 
 const checkListRouter = require('./src/routes/checklist')
+const taskRouter = require('./src/routes/task')
 const rootRouter = require('./src/routes/index')
 const methodOverride = require('method-override')
 
@@ -19,6 +20,7 @@ app.set('view engine', 'ejs')
 
 app.use('/', rootRouter)
 app.use('/checklist', checkListRouter)
+app.use('/checklist', taskRouter.checklistDependent)
 
 app.listen(3000, () => {
     console.log('Servidor iniciado!')
